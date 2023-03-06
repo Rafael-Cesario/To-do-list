ad658ff35bff73673f3d410d39c5132a610fa536
Rafael <rafaeloliveiracds@gmail.com>
2023-03-06 00:19:12 -0300

    [ feat ] apollo formatted error

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { GraphQLFormattedError } from 'graphql';
 import { schema } from './schemas/schemas';
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+	schema,
+	formatError: (formattedError: GraphQLFormattedError) => ({ message: formattedError.message }),
+});
 
 export const startServer = async () => {
 	const { url } = await startStandaloneServer(server, {
