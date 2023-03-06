50ff45b9f5ef4523fb4cc8e71b4c0085a05d1e15
Rafael <rafaeloliveiracds@gmail.com>
2023-03-06 02:50:02 -0300

    [ tests ] create user

--- src/__tests__/createUser.spec.ts
+++ src/__tests__/createUser.spec.ts
@@ -1,7 +1,40 @@
-import { describe, it, expect } from 'vitest';
+import mongoose from 'mongoose';
+import request from 'supertest-graphql';
+import gql from 'graphql-tag';
+import { startServer } from '../server';
+import { describe, it, expect, beforeAll, afterAll, afterEach } from 'vitest';
+import { startDatabase } from '../database';
+
+const CREATE_USER = gql`
+	mutation CreateUser($user: InputUser!) {
+		createUser(user: $user) {
+			message
+		}
+	}
+`;
 
 describe('Create User', () => {
-	it('Creates a new user', () => {
-		expect(2).toBe(1);
+	let url: string;
+
+	beforeAll(async () => {
+		url = await startServer(0);
+		startDatabase();
+	});
+
+	afterEach(async () => {
+		await mongoose.connection.dropDatabase();
+	});
+
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+
+	it('Creates a new user', async () => {
+		type Response = { createUser: { message: string } };
+		const { data } = await request<Response>(url)
+			.mutate(CREATE_USER)
+			.variables({ user: { email: 'Rafael@hotmail.com', name: 'Rafael', password: 'Rafael123' } });
+
+		expect(data?.createUser.message).toBe('Success: New user created');
 	});
 });
