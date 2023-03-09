f1f3d4a4b08a9454f65870d3307baf2b89809811
Rafael <rafaeloliveiracds@gmail.com>
2023-03-09 04:17:33 -0300

    [ test refactor ] function to create a user

--- Backend/src/__tests__/user/createUser.spec.ts
+++ Backend/src/__tests__/user/createUser.spec.ts
@@ -1,30 +1,18 @@
 import mongoose from 'mongoose';
-import request from 'supertest-graphql';
-import { startServer } from '../../server';
 import { describe, it, expect, beforeAll, afterAll, afterEach } from 'vitest';
+import { startServer } from '../../server';
 import { startDatabase } from '../../database';
-import { InputCreateUser } from '../../interfaces/interfacesUser';
 import { ModelUser } from '../../models/modelUser';
-import { CREATE_USER } from '../__queries__/queriesUser';
+import { requestCreateUser } from '../__queries__/queriesUser';
+
+const defaultUser = { email: 'email@user.com', name: 'userName', password: 'strongPassword' };
 
 describe('Create User', () => {
 	let url: string;
 
-	const requestCreateUser = async (createUser?: { createUser: InputCreateUser }) => {
-		type Response = { createUser: { message: string } };
-
-		const { data, errors } = await request<Response>(url)
-			.mutate(CREATE_USER)
-			.variables(
-				createUser ? { createUser } : { createUser: { email: 'UserEmail', name: 'name', password: 'password' } }
-			);
-
-		return { data, errors };
-	};
-
 	beforeAll(async () => {
 		url = await startServer(0);
-		startDatabase();
+		await startDatabase();
 	});
 
 	afterEach(async () => {
@@ -37,12 +25,12 @@ describe('Create User', () => {
 	});
 
 	it('Creates a new user', async () => {
-		const { data } = await requestCreateUser();
+		const { data } = await requestCreateUser(url, defaultUser);
 		expect(data?.createUser.message).toBe('Success: New user created');
 	});
 
 	it('Throws a error. empty variables', async () => {
-		const { errors } = await requestCreateUser({ createUser: { email: '', name: '', password: '' } });
+		const { errors } = await requestCreateUser(url, { email: '', name: '', password: '' });
 
 		expect(errors?.[0].message).toMatch(
 			/name: Name is required, email: Email is required, password: Password is required/
@@ -50,37 +38,30 @@ describe('Create User', () => {
 	});
 
 	it('Throws a error. Email is duplicated', async () => {
-		await request(url)
-			.mutate(CREATE_USER)
-			.variables({ user: { email: 'UserEmail', name: 'name', password: 'password' } });
-
-		const { errors } = await request(url)
-			.mutate(CREATE_USER)
-			.variables({ user: { email: 'UserEmail', name: 'name', password: 'password' } });
+		await requestCreateUser(url, defaultUser);
+		const { errors } = await requestCreateUser(url, defaultUser);
 
 		expect(errors?.[0].message).toMatch(/This user already exist/i);
 	});
 
 	it('Throws a error. Name is to long', async () => {
-		const { errors } = await requestCreateUser({
-			createUser: {
-				email: 'email',
-				name: 'this name is way to long',
-				password: 'password',
-			},
+		const { errors } = await requestCreateUser(url, {
+			email: 'email',
+			name: 'this name is way to long',
+			password: 'password',
 		});
 
 		expect(errors?.[0].message).toMatch(/longer than the maximum allowed length/);
 	});
 
 	it('hash the password', async () => {
-		await requestCreateUser();
-		const user = await ModelUser.findOne({ email: 'email' });
-		expect(user?.password).not.toBe('email');
+		await requestCreateUser(url, defaultUser);
+		const user = await ModelUser.findOne({ email: defaultUser.email });
+		expect(user?.password).not.toBe(defaultUser.password);
 	});
 
 	it('saves email as lowerCase', async () => {
-		await requestCreateUser({ createUser: { email: 'USEREMAIL', name: 'name', password: 'password' } });
+		await requestCreateUser(url, { email: 'USEREMAIL', name: 'name', password: 'password' });
 		const user = await ModelUser.findOne({ email: 'useremail' });
 		expect(user?.email).toBe('useremail');
 	});
