751ecf5112da980c10a51b89d33a55cb993e30cf
Rafael <rafaeloliveiracds@gmail.com>
2023-03-13 20:01:44 -0300

    [ test ] rename todo

--- Backend/src/__tests__/__queries__/queriesTodos.ts
+++ Backend/src/__tests__/__queries__/queriesTodos.ts
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 import request from 'supertest-graphql';
-import { InputCreateTodo, InputReadTodos } from '../../interfaces/interfacesTodo';
+import { InputCreateTodo, InputReadTodos, InputRenameTodo } from '../../interfaces/interfacesTodo';
 
 interface ResponseCreateTodo {
 	createTodo: { message: string };
@@ -8,6 +8,10 @@ interface ResponseCreateTodo {
 
 interface ResponseReadTodos {
 	readTodos: { id: string; task: string; tags: string[]; status: string }[];
+}
+
+interface ResponseRenameTodo {
+	renameTodo: { message: string };
 }
 
 const CREATE_TODO = gql`
@@ -29,6 +33,14 @@ const READ_TODOS = gql`
 	}
 `;
 
+const RENAME_TODO = gql`
+	mutation RenameTodo($renameTodo: InputRenameTodo!) {
+		renameTodo(renameTodo: $renameTodo) {
+			message
+		}
+	}
+`;
+
 export const requestCreateTodo = async (url: string, createTodo: InputCreateTodo) => {
 	const { data, errors } = await request<ResponseCreateTodo>(url).mutate(CREATE_TODO).variables({ createTodo });
 	return { data, error: errors?.[0].message };
@@ -36,5 +48,10 @@ export const requestCreateTodo = async (url: string, createTodo: InputCreateTodo
 
 export const requestReadTodos = async (url: string, readTodos: InputReadTodos) => {
 	const { data, errors } = await request<ResponseReadTodos>(url).query(READ_TODOS).variables({ readTodos });
+	return { data, error: errors?.[0].message };
+};
+
+export const requestRenameTodo = async (url: string, renameTodo: InputRenameTodo) => {
+	const { data, errors } = await request<ResponseRenameTodo>(url).mutate(RENAME_TODO).variables({ renameTodo });
 	return { data, error: errors?.[0].message };
 };
