e2ce0682c97de2cbda82969b2bd1168b838be3b7
Rafael <rafaeloliveiracds@gmail.com>
2023-03-14 19:14:56 -0300

    [ test ] Delete tag

--- Backend/src/__tests__/__queries__/queriesTags.ts
+++ Backend/src/__tests__/__queries__/queriesTags.ts
@@ -1,6 +1,6 @@
 import request from 'supertest-graphql';
 import gql from 'graphql-tag';
-import { InputCreateTag, InputUpdateTag } from '../../interfaces/interfacesTags';
+import { InputCreateTag, InputDeleteTag, InputUpdateTag } from '../../interfaces/interfacesTags';
 
 interface ResponseCreateTag {
 	createTag: { message: string };
@@ -8,6 +8,10 @@ interface ResponseCreateTag {
 
 interface ResponseUpdateTag {
 	updateTag: { message: string };
+}
+
+interface ResponseDeleteTag {
+	deleteTag: { message: string };
 }
 
 const CREATE_TAG = gql`
@@ -26,6 +30,14 @@ const UPDATE_TAG = gql`
 	}
 `;
 
+const DELETE_TAG = gql`
+	mutation DeleteTag($deleteTag: InputDeleteTag!) {
+		deleteTag(deleteTag: $deleteTag) {
+			message
+		}
+	}
+`;
+
 export const requestCreateTag = async (url: string, createTag: InputCreateTag) => {
 	const { data, errors } = await request<ResponseCreateTag>(url).mutate(CREATE_TAG).variables({ createTag });
 	return { data: data?.createTag.message, error: errors?.[0].message };
@@ -34,4 +46,9 @@ export const requestCreateTag = async (url: string, createTag: InputCreateTag) =
 export const requestUpdateTag = async (url: string, updateTag: InputUpdateTag) => {
 	const { data, errors } = await request<ResponseUpdateTag>(url).mutate(UPDATE_TAG).variables({ updateTag });
 	return { data: data?.updateTag.message, error: errors?.[0].message };
+};
+
+export const requestDeleteTag = async (url: string, deleteTag: InputDeleteTag) => {
+	const { data, errors } = await request<ResponseDeleteTag>(url).mutate(DELETE_TAG).variables({ deleteTag });
+	return { data: data?.deleteTag.message, error: errors?.[0].message };
 };
