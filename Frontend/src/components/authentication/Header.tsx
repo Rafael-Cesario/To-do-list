de521b645fd3f7cc98ba0e93022d70f333c71d11
Rafael <rafaeloliveiracds@gmail.com>
2023-03-16 17:48:16 -0300

    [ feat ] active form state

--- Frontend/src/components/authentication/Header.tsx
+++ Frontend/src/components/authentication/Header.tsx
@@ -1,10 +1,20 @@
-import { StyledHeader } from "./styles/StyledHeader";
+import { useState } from 'react';
+import { StyledHeader } from './styles/StyledHeader';
 
 export const Header = () => {
+  const [activeForm, setActiveForm] = useState('login');
+
   return (
     <StyledHeader>
-      <button className="active">Login</button>
-      <button>Criar uma conta</button>
+      <button className={activeForm === 'login' ? 'active' : ''} onClick={() => setActiveForm(activeForm === 'login' ? '' : 'login')}>
+        Login
+      </button>
+
+      <button
+        className={activeForm === 'createAccount' ? 'active' : ''}
+        onClick={() => setActiveForm(activeForm === 'createAccount' ? '' : 'createAccount')}>
+        Criar uma conta
+      </button>
     </StyledHeader>
   );
 };
