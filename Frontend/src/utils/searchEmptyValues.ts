dcb52732c651d87035608e158f93cf638397fb49
Rafael <rafaeloliveiracds@gmail.com>
2023-03-17 17:39:57 -0300

    test // Search empty values

--- Frontend/src/utils/searchEmptyValues.ts
+++ Frontend/src/utils/searchEmptyValues.ts
@@ -1,16 +1,14 @@
-export const searchEmptyValues = (values: object) => {
+export const searchEmptyValues = (values: { [key: string]: string }) => {
   type TKey = keyof typeof values;
 
   const keys = Object.keys(values);
   const emptyValues: { [key: string]: string } = {};
 
   keys.forEach((key) => {
-    const emptyValue = values[key as TKey] === '';
+    const emptyValue = values[key as TKey].trim() === '';
     if (emptyValue) emptyValues[key as TKey] = `Este campo não pode ficar vazio.`;
   });
 
   const hasEmptyValues = Object.keys(emptyValues).length !== 0;
   return hasEmptyValues && emptyValues;
 };
-
-// todo > tests
