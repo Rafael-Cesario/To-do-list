4ad10412e85bdff6a35c541e1332204435dac606
Rafael <rafaeloliveiracds@gmail.com>
2023-03-23 21:27:06 -0300

    feat // input get list name

--- Frontend/src/features/index/CreateList.tsx
+++ Frontend/src/features/index/CreateList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StyleCreateList } from './styles/StyledCreateList';
 
 interface Props {
@@ -7,13 +8,27 @@ interface Props {
 }
 
 export const CreateList = ({ props: { setShowCreateNewList } }: Props) => {
+  const [listName, setListName] = useState('');
+
+  const createList = () => {
+    console.log({ listName });
+  };
+
   return (
     <StyleCreateList>
       <h1 className="title">Criar nova lista</h1>
-      <input className="list-name" type="text" placeholder="Nova lista" />
+
+      <input
+        type="text"
+        placeholder="Nova lista"
+        className="list-name"
+        value={listName}
+        onChange={(e) => setListName(e.target.value)}
+        onKeyUp={(e) => e.key === 'Enter' && createList()}
+      />
 
       <div className="buttons">
-        <button>Criar</button>
+        <button onClick={() => createList()}>Criar</button>
         <button onClick={() => setShowCreateNewList(false)}>Cancelar</button>
       </div>
     </StyleCreateList>
