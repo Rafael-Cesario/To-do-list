d4bb1d974330b3b19518c306b414e97c2b13ef83
Rafael <rafaeloliveiracds@gmail.com>
2023-03-24 19:07:58 -0300

    Feat // Load todos

--- Frontend/src/utils/hooks/useQueriesTodos.tsx
+++ Frontend/src/utils/hooks/useQueriesTodos.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { sliceTodos } from '../../features/todolist/utils/sliceTodos';
 import { useMutation, useQuery } from '@apollo/client';
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { CREATE_TODO, READ_TODOS, InputReadTodos, ITodoModel } from '../interfaces/interfaceQueriesTodos';
 import { localStorageKeys } from '../localStorageKeys';
 import { errors } from '../requestErrors';
+import { Store } from '../store';
 
 export const useQueriesTodos = () => {
   const [email, setEmail] = useState('');
   const { listName } = useParams();
   const [mutationCreateTodo] = useMutation(CREATE_TODO);
+
+  const { todos } = useSelector((state: Store) => state.todos);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const storage: { email: string } = JSON.parse(localStorage.getItem(localStorageKeys.user) || '');
@@ -25,9 +31,23 @@ export const useQueriesTodos = () => {
     },
   });
 
+  useEffect(() => {
+    const todos = data?.readTodos || [];
+    dispatch(sliceTodos.actions.loadTodos({ todos }));
+  }, [data]);
+
   const requestCreateTodo = async (createTodo: { id: string; task: string }) => {
     try {
-      const { data } = await mutationCreateTodo({ variables: { createTodo: { ...createTodo, email, listName } } });
+      const { data } = await mutationCreateTodo({
+        variables: { createTodo: { ...createTodo, email, listName } },
+      });
+
+      dispatch(
+        sliceTodos.actions.createTodo({
+          todo: { ...createTodo, tags: [], status: 'next' },
+        })
+      );
+
       return { data };
     } catch (error: any) {
       console.log({ error: error.message });
@@ -36,5 +56,5 @@ export const useQueriesTodos = () => {
     }
   };
 
-  return { requestCreateTodo, todos: data?.readTodos ?? [], error, loading };
+  return { requestCreateTodo, error, loading, todos };
 };
