09ec4d13d8f4c685d5219c779838e4cbf216dfb1
Rafael <rafaeloliveiracds@gmail.com>
2023-03-31 01:31:10 -0300

    Database name

--- Backend/src/database.ts
+++ Backend/src/database.ts
@@ -1,13 +1,18 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-const URI = `${process.env.DATABASE}_${process.env.NODE_ENV}`;
+let URI = process.env.DATABASE;
+
+if (process.env.NODE_ENV) {
+	URI += `-${process.env.NODE_ENV}`;
+}
 
 export const startDatabase = async () => {
 	try {
 		mongoose.set('strictQuery', false);
-		await mongoose.connect(URI);
+		await mongoose.connect(URI!);
 
 		const testEnvironment = process.env.NODE_ENV === 'test';
 		testEnvironment || console.log('DB connected');
