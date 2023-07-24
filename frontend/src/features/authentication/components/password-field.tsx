ffcb891cc3bf272e3656c2568c9f81a6a5550e9e
Rafael <rafaeloliveiracds@gmail.com>
2023-07-24 11:33:48 -0300

    [ Feat ] Authentication - Show and hide password

--- frontend/src/features/authentication/components/password-field.tsx
+++ frontend/src/features/authentication/components/password-field.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { StyledField } from "../styles/field-style";
+import { useState } from "react";
 
 interface IPasswordField {
 	name: string;
@@ -8,13 +9,36 @@ interface IPasswordField {
 }
 
 export const PasswordField = ({ name, label, placeholder }: IPasswordField) => {
+	const [showPassword, setShowPassword] = useState(false);
+
 	return (
 		<StyledField>
 			<label htmlFor={name}>{label}</label>
 
 			<div className="input-password">
-				<input type="password" id={name} placeholder={placeholder}/>
-				<Image className="icon" alt="input-icon" src={"/icons/eye-closed.png"} width={20} height={20} />
+				<input type={showPassword ? "text" : "password"} id={name} placeholder={placeholder} />
+
+				{showPassword || (
+					<Image
+						onClick={() => setShowPassword(!showPassword)}
+						className="icon"
+						alt="input-icon"
+						src={"/icons/eye-closed.png"}
+						width={20}
+						height={20}
+					/>
+				)}
+
+				{showPassword && (
+					<Image
+						onClick={() => setShowPassword(!showPassword)}
+						className="icon"
+						alt="input-icon"
+						src={"/icons/eye-open.png"}
+						width={20}
+						height={20}
+					/>
+				)}
 			</div>
 		</StyledField>
 	);
