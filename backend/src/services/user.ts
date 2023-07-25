3a1eeb537c6ac51148b515518d28395e777285fb
Rafael <rafaeloliveiracds@gmail.com>
2023-07-25 07:02:43 -0300

    [ Feat ] Backend - Create user

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,11 +1,21 @@
 import { INewUser } from "../interfaces/user";
 import { GraphQLError } from "graphql";
 import { searchEmptyValues } from "../utils/search-empty-values";
+import { encryptPassword } from "../utils/crypt";
+import { prisma } from "../database";
 
 class UserServices {
-	createUser({ newUser }: INewUser) {
+	async createUser({ newUser }: INewUser) {
 		const hasEmptyValues = searchEmptyValues(newUser);
 		if (hasEmptyValues) throw new GraphQLError(hasEmptyValues);
+
+		const isDuplicatedUser = await prisma.user.findUnique({ where: { email: newUser.email } });
+		if (isDuplicatedUser) throw new GraphQLError("A user with the same email already exist");
+
+		newUser.password = encryptPassword(newUser.password);
+		await prisma.user.create({ data: newUser });
+
+		return { message: "Success: A new user was created" };
 	}
 }
 
---
+++ backend/src/utils/crypt.ts
@@ -0,0 +1,8 @@
+import { genSaltSync, hashSync } from "bcrypt";
+
+export const encryptPassword = (password: string) => {
+	const saltRounds = 10;
+	const salt = genSaltSync(saltRounds);
+	const hash = hashSync(password, salt);
+	return hash;
+};
