2d89a04e78fdc907d05b1f7279683d4d10f1a83e
Rafael <rafaeloliveiracds@gmail.com>
2023-08-03 01:50:34 -0300

    [ Feat ] Push new list to the global state

--- frontend/src/features/home/components/create-list.tsx
+++ frontend/src/features/home/components/create-list.tsx
@@ -9,6 +9,7 @@ import { useDispatch } from "react-redux";
 import { setNotification } from "@/context/slice-notification";
 import { showError } from "@/utils/show-error";
 import { errorsMap } from "@/services/errors-map";
+import { addList } from "../context/list-slice";
 
 export const CreateList = () => {
 	const [createListContainer, setCreateListContainer] = useState(false);
@@ -28,14 +29,16 @@ export const CreateList = () => {
 		setLoading(true);
 
 		try {
+			// todo > Set the new list as active
 			const userCookies: IUserCookies = await cookies.get("user");
 			const input = { userID: userCookies.userID, name: listName };
-			await createListRequest({ input });
-			dispatch(setNotification({ isOpen: true, type: "success", title: "Nova lista criada", message: "Sua nova lista foi criada com sucesso" }));
-			setListName("");
 
-			// TODO > Push the new list to the global state
-			// todo > Set the new list as current active
+			const { data } = await createListRequest({ input });
+			if (!data) throw new Error("Data is undefined");
+
+			dispatch(setNotification({ isOpen: true, type: "success", title: "Nova lista criada", message: "Sua nova lista foi criada com sucesso" }));
+			dispatch(addList({ list: data.createList }));
+			setListName("");
 		} catch (error: any) {
 			showError(error, dispatch, errorsMap.list);
 		}
