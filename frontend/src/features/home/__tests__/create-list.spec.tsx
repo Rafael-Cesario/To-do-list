6b972d1503203828240499f08c59427547f406c6
Rafael <rafaeloliveiracds@gmail.com>
2023-08-03 09:38:59 -0300

    [ Test ] Sidebar - Create list - pre test

---
+++ frontend/src/features/home/__tests__/create-list.spec.tsx
@@ -0,0 +1,48 @@
+import { Notification } from "@/components/notification";
+import { Sidebar } from "../sidebar";
+import { AllProviders } from "@/lib/all-providers";
+import { render, screen } from "@testing-library/react";
+import { store } from "@/context/store";
+import { setLists } from "../context/list-slice";
+import { act } from "react-dom/test-utils";
+
+const Component = () => (
+	<AllProviders>
+		<Notification />
+		<Sidebar />
+	</AllProviders>
+);
+
+describe("Home - Sidebar - Create list", () => {
+	beforeAll(() => {
+		render(<Component />);
+		act(() => store.dispatch(setLists({ lists: [{ listID: "unique", userID: "unique", name: "list01" }] })));
+	});
+
+	it("Open and close create list container", async () => {
+		screen.debug();
+	});
+
+	it.todo("Create a new list and show on the sidebar", async () => {
+		// reset listname
+		// show a notification
+		// show list on the page
+		// set list as active
+	});
+
+	it.todo("Show a error if user try to create a list without a name");
+
+	it.todo("Show a notification due to request error");
+});
+
+vi.mock("@/services/cookies", () => ({
+	get: () => ({ userID: "123" }),
+}));
+
+vi.mock("@/utils/hooks/use-mutations-list", () => ({
+	useMutationsList: () => ({
+		createListRequest: (input: { name: string }) => ({
+			data: { createList: { listID: "123", userID: "123", name: input.name } },
+		}),
+	}),
+}));
