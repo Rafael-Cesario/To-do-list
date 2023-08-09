5a8df7c83da8a0f1106750708b75fd873f334502
Rafael <rafaeloliveiracds@gmail.com>
2023-08-09 20:38:07 -0300

    [ Feat ] create subject request

--- frontend/src/features/home/components/create-subject.tsx
+++ frontend/src/features/home/components/create-subject.tsx
@@ -1,14 +1,42 @@
 "use client";
 import { useState } from "react";
 import { StyledCreateSubject } from "../styles/create-subject-style";
+import { useMutationsSubject } from "@/utils/hooks/use-mutations-subject";
+import { useSelector } from "react-redux";
+import { Store } from "@/context/store";
 
 export const CreateSubject = () => {
 	const [subjectName, setSubjectName] = useState("");
 	const [feedback, setFeedback] = useState({ type: "", message: "" });
+	const [loading, setLoading] = useState(false);
+	const { active } = useSelector((state: Store) => state.list);
+	const { createSubjectRequest } = useMutationsSubject();
 
-	const createSubject = () => {
+	const createSubject = async () => {
 		if (!subjectName) return setFeedback({ type: "error", message: "Escolha um nome ou titulo antes de clicar em adicionar" });
-		setFeedback({ type: "success", message: "Seu novo assunto foi adicionado" });
+
+		setLoading(true);
+
+		// todo
+		// check if subject exist, if true increase the subject amount
+		// return
+
+		try {
+			const { data } = await createSubjectRequest({ input: { listID: active.listID, name: subjectName.trim().toLowerCase() } });
+			console.log({ data });
+
+			setFeedback({ type: "success", message: "Seu novo assunto foi adicionado" });
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			console.log(error.message);
+			setFeedback({ type: "error", message: "Ops, um erro inesperado ocorreu, por favor recarregue a pagina e tente novamente" });
+		}
+
+		setLoading(false);
+
+		setTimeout(() => {
+			setFeedback({ type: "", message: "" });
+		}, 2000);
 	};
 
 	return (
