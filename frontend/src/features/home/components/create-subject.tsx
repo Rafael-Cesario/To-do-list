47d11286455d04b0b5787c0ab70d1fc7197ab7ef
Rafael <rafaeloliveiracds@gmail.com>
2023-08-09 20:01:06 -0300

    [ Feat ] Create subject feedback message

--- frontend/src/features/home/components/create-subject.tsx
+++ frontend/src/features/home/components/create-subject.tsx
@@ -1,10 +1,40 @@
+"use client";
+import { useState } from "react";
 import { StyledCreateSubject } from "../styles/create-subject-style";
 
 export const CreateSubject = () => {
+	const [subjectName, setSubjectName] = useState("");
+	const [feedback, setFeedback] = useState({ type: "", message: "" });
+
+	const createSubject = () => {
+		if (!subjectName) return setFeedback({ type: "error", message: "Escolha um nome ou titulo antes de clicar em adicionar" });
+		setFeedback({ type: "success", message: "Seu novo assunto foi adicionado" });
+	};
+
 	return (
 		<StyledCreateSubject>
-			<input className="name" type="text" placeholder="Digite aqui seu novo assunto de estudos" />
-			<button className="submit">Adicionar</button>
+			{feedback.message && (
+				<div className={feedback.type}>
+					<span className="message">{feedback.message}</span>
+					<button onClick={() => setFeedback({ type: "", message: "" })} className="close">
+						x
+					</button>
+				</div>
+			)}
+
+			<div className="create">
+				<input
+					value={subjectName}
+					onChange={(e) => setSubjectName(e.target.value)}
+					onKeyUp={(e) => e.key === "Enter" && createSubject()}
+					className="name"
+					type="text"
+					placeholder="Digite aqui seu novo assunto de estudos"
+				/>
+				<button onClick={() => createSubject()} className="submit">
+					Adicionar
+				</button>
+			</div>
 		</StyledCreateSubject>
 	);
 };
--- frontend/src/features/home/styles/create-subject-style.tsx
+++ frontend/src/features/home/styles/create-subject-style.tsx
@@ -4,8 +4,11 @@ import styled from "styled-components";
 
 export const StyledCreateSubject = styled.div`
 	margin: 0 2rem;
-	display: flex;
 	box-shadow: 10px 10px 10px #00000010;
+
+	.create {
+		display: flex;
+	}
 
 	.name {
 		margin-right: 1rem;
@@ -17,5 +20,41 @@ export const StyledCreateSubject = styled.div`
 		border: 2px solid transparent;
 		font-weight: bold;
 		padding: 0 1rem;
+	}
+
+	.error,
+	.success {
+		font-size: 0.8rem;
+		width: fit-content;
+		color: #ddd;
+		border-radius: ${Palette.borderRadius};
+		overflow: hidden;
+
+		.message {
+			margin: 1rem 8px;
+		}
+
+		.close {
+			border: 2px solid transparent;
+			margin: 4px;
+			margin-left: 1rem;
+			background-color: #222;
+			color: #ddd;
+			font-size: 1rem;
+			padding: 0 1rem;
+
+			&:hover {
+				background-color: #ddd;
+				color: #222;
+			}
+		}
+	}
+
+	.error {
+		background-color: ${Palette.errorText};
+	}
+
+	.success {
+		background-color: ${Palette.successText};
 	}
 `;
