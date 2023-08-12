13d5f073948fa017664a919f4d97d31a919b9115
Rafael <rafaeloliveiracds@gmail.com>
2023-08-11 21:07:15 -0300

    [ Feat ] Subject area

---
+++ frontend/src/features/home/styles/subject-area/subject-area-style.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { Palette } from "@/styles/palette";
+import styled from "styled-components";
+
+export const StyledSubjectArea = styled.div`
+	position: absolute;
+	z-index: 1;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: #00000080;
+	backdrop-filter: blur(2px);
+	display: flex;
+	justify-content: flex-end;
+
+	.container {
+		margin: 1rem;
+		border-radius: ${Palette.borderRadius};
+		border: 2px solid ${Palette.borderContainer};
+		width: 30vw;
+		max-width: 1000px;
+		min-width: 400px;
+		min-height: 100vh;
+		background-color: ${Palette.container};
+		padding: 1rem 2rem;
+
+		.close {
+			position: absolute;
+			right: 0;
+			margin: 0 2rem;
+			border: none;
+			color: ${Palette.fadedText};
+			font-size: 1rem;
+
+			&:hover {
+				background-color: ${Palette.errorText};
+				color: #ddd;
+			}
+		}
+
+		.details {
+			display: flex;
+			justify-content: space-between;
+			color: ${Palette.fadedText};
+			font-size: 0.8rem;
+		}
+
+		.notes {
+			margin: 4rem 0;
+
+			.title {
+				margin-bottom: 1rem;
+			}
+
+			textarea {
+				width: 100%;
+				min-height: 100px;
+				resize: vertical;
+				padding: 1rem;
+				background-color: ${Palette.background};
+				border: 2px solid ${Palette.borderContainer};
+				box-shadow: 2px 2px 2px #00000050;
+				color: #aaa;
+
+				&:focus {
+					outline: 2px solid ${Palette.primary};
+				}
+			}
+		}
+	}
+`;
--- frontend/src/features/home/subject-area.tsx
+++ frontend/src/features/home/subject-area.tsx
@@ -1,7 +1,27 @@
+import { StyledSubjectArea } from "./styles/subject-area/subject-area-style";
+
 export const SubjectArea = () => {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<StyledSubjectArea>
+			<div className="container">
+				<button className="close">x</button>
+
+				<h1 className="title">Hello</h1>
+
+				<div className="details">
+					<span>Adicionado 4x</span>
+					<span>11/08/2023</span>
+				</div>
+
+				<div className="notes">
+					<h2 className="title">Anotações</h2>
+					<textarea name="notes" placeholder="Adicione suas anotações aqui. Ex: Links"></textarea>
+				</div>
+
+				{/* tags */}
+				{/* Delete subject */}
+				{/* Save changes */}
+			</div>
+		</StyledSubjectArea>
 	);
 };
