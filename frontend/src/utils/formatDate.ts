7e9986b012936674e36e650659a04b5a9b688565
Rafael <rafaeloliveiracds@gmail.com>
2023-08-17 11:02:17 -0300

    [ Fix ] format date get day

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,6 +1,6 @@
 export const formatDate = (textDate: string) => {
 	const date = new Date(Number(textDate));
-	const day = String(date.getDay()).padStart(2, "0");
+	const day = String(date.getDate()).padStart(2, "0");
 	const month = String(date.getMonth() + 1).padStart(2, "0");
 	const subjectDate = `${day}/${month}/${date.getFullYear()}`;
 	return subjectDate;
