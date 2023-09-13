58ff87917bcdb30afbc22d7296a147d65df5f099
Rafael <rafaeloliveiracds@gmail.com>
2023-09-12 21:28:31 -0300

    [ Test ] List e2e - Get lists

--- backend/test/list.e2e-spec.ts
+++ backend/test/list.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from 'src/app.module';
 import { UserModel } from 'src/models/user/user.model';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { listQueries } from './queries/list';
-import { CreateListInput } from 'src/models/list/list.dto';
+import { CreateListInput, GetListInput } from 'src/models/list/list.dto';
 import { ListModel } from 'src/models/list/list.models';
 
 describe('Lists', () => {
@@ -22,6 +22,11 @@ describe('Lists', () => {
     return { data, error: errors?.[0] };
   };
 
+  const requestGetLists = async (getListData: GetListInput) => {
+    const { data, errors } = await request<{ getLists: ListModel }>(app.getHttpServer()).query(listQueries.GET_LISTS).variables({ getListData });
+    return { data, error: errors?.[0].message };
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -31,16 +36,16 @@ describe('Lists', () => {
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.init();
-    await createUser();
-  });
-
-  afterEach(async () => {
-    await prisma.user.deleteMany();
   });
 
   describe('Create List', () => {
+    beforeEach(async () => {
+      await createUser();
+    });
+
     afterEach(async () => {
       await prisma.list.deleteMany();
+      await prisma.user.deleteMany();
     });
 
     it('Create a new list', async () => {
@@ -64,6 +69,33 @@ describe('Lists', () => {
     it('Throws a error due to empty values', async () => {
       const { error } = await requestCreateList({ name: '', userID: '' });
       expect(error.message).toEqual(['name must be longer than or equal to 3 characters', 'userID should not be empty']);
+    });
+  });
+
+  describe('Get lists', () => {
+    beforeAll(async () => {
+      await createUser();
+      await requestCreateList({ name: 'list', userID: user.id });
+    });
+
+    afterAll(async () => {
+      await prisma.list.deleteMany();
+      await prisma.user.deleteMany();
+    });
+
+    it('Get all lists', async () => {
+      const { data } = await requestGetLists({ userID: user.id });
+      expect(data.getLists).toHaveLength(1);
+    });
+
+    it('Throws an error due to empty values', async () => {
+      const { error } = await requestGetLists({ userID: '' });
+      expect(error[0]).toBe('userID should not be empty');
+    });
+
+    it('Throws an error due to wrong userID', async () => {
+      const { error } = await requestGetLists({ userID: 'wrong' });
+      expect(error).toBe('notFound: User not found');
     });
   });
 });
--- backend/test/queries/list.ts
+++ backend/test/queries/list.ts
@@ -10,6 +10,30 @@ class ListQueries {
       }
     }
   `;
+
+  readonly GET_LISTS = gql`
+    query GetLists($getListData: GetListInput!) {
+      getLists(getListData: $getListData) {
+        id
+        name
+        userID
+        tasks {
+          createdAt
+          description
+          id
+          listID
+          status
+          title
+          tags {
+            color
+            id
+            name
+            taskId
+          }
+        }
+      }
+    }
+  `;
 }
 
 export const listQueries = new ListQueries();
