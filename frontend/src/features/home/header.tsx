0940e602a7c22d11685e15f75f9a23bcee2ea36d
Rafael <rafaeloliveiracds@gmail.com>
2023-09-25 13:53:55 -0300

    [ Style ] Header

--- frontend/src/features/home/header.tsx
+++ frontend/src/features/home/header.tsx
@@ -3,7 +3,16 @@ import { StyledHeader } from "./styles/styled-header";
 export const Header = () => {
 	return (
 		<StyledHeader>
-			<h1>Header</h1>
+			<div className="active">
+				<h1 className="title">ListName</h1>
+				<span className="tasks-length">20 itens na lista</span>
+			</div>
+
+			<div className="right">
+				<input className="search-task" type="text" placeholder="Buscar tarefa, tag, ou data" />
+				<button className="configs">Configurações</button>
+				<button className="create-task">Adicionar tarefa</button>
+			</div>
 		</StyledHeader>
 	);
 };
--- frontend/src/features/home/styles/styled-header.tsx
+++ frontend/src/features/home/styles/styled-header.tsx
@@ -1,6 +1,46 @@
 "use client";
+import { Palette } from "@/styles/palette";
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
 	grid-area: 1 / 2;
+	margin: 1rem 2rem;
+
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+
+	.active {
+		.title {
+			color: ${Palette.primary};
+		}
+
+		.tasks-length {
+			color: ${Palette.text + "90"};
+			font-size: 0.8rem;
+		}
+	}
+
+	.right {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		max-width: 50vw;
+		flex-grow: 1;
+
+		.search-task {
+			flex-grow: 1;
+		}
+
+		.configs {
+			background-color: transparent;
+			color: ${Palette.text + "90"};
+			margin: 0 2rem;
+
+			&:hover {
+				background-color: ${Palette.container};
+				color: ${Palette.text};
+			}
+		}
+	}
 `;
