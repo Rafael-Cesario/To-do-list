ae798c7b51305cea8858c8e4e0d34ca3ab660c2e
Rafael <rafaeloliveiracds@gmail.com>
2023-10-03 18:32:20 -0300

    [ Refactor ] Task container

---
+++ frontend/src/features/home/components/main/styles/styled-task-container.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { Palette } from "@/styles/palette";
+import styled from "styled-components";
+
+export const StyledTaskContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, min(500px));
+	column-gap: 1rem;
+	row-gap: 1rem;
+	justify-content: center;
+	height: fit-content;
+	margin: 4rem 1rem;
+
+	.task {
+		border-radius: ${Palette.borderRadius};
+		background-color: ${Palette.container};
+		padding: 1rem;
+		max-width: 500px;
+		width: 100%;
+		min-height: 200px;
+	}
+
+	.top {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 1rem;
+
+		.date {
+			font-size: 0.8rem;
+			color: ${Palette.text + "90"};
+		}
+	}
+
+	.title {
+		text-transform: capitalize;
+		font-size: 1.2rem;
+	}
+
+	.description {
+		margin-bottom: 1rem;
+		height: 85px;
+		overflow: hidden;
+	}
+
+	.status,
+	.tag {
+		padding: 2px 8px;
+		border-radius: ${Palette.borderRadius};
+		margin: 4px;
+		font-size: 0.8rem;
+		color: #111;
+		font-weight: bold;
+	}
+
+	.status {
+		color: #eee;
+		font-weight: normal;
+	}
+
+	.next {
+		background-color: ${Palette.insideContainer};
+	}
+
+	.current {
+		background-color: ${Palette.primary};
+	}
+
+	.done {
+		background-color: ${Palette.success};
+	}
+
+	.tags {
+		display: flex;
+		flex-wrap: wrap;
+		margin-top: 1rem;
+
+		.tag {
+			flex-grow: 1;
+		}
+	}
+`;
--- frontend/src/features/home/main.tsx
+++ frontend/src/features/home/main.tsx
@@ -1,79 +1,11 @@
-"use client";
-import { useDispatch, useSelector } from "react-redux";
 import { StyledMain } from "./styles/styled-main";
-import { Store } from "@/context/store";
-import { tagColors } from "@/styles/palette";
-import { ITask } from "@/services/interfaces/task";
-import { setActive } from "./context/task-slice";
+import { TaskContainer } from "./components/main/task-container";
 
 export const Main = () => {
-	const { active } = useSelector((state: Store) => state.list);
-	const { filter } = useSelector((state: Store) => state.task);
-
-	const dispatch = useDispatch();
-
-	const statusMap = {
-		NEXT: "Próximas",
-		CURRENT: "Em Progresso",
-		DONE: "Concluídas",
-	};
-
-	const formatDate = (date: Date) => {
-		const fullDate = new Date(date);
-
-		const day = fullDate.getDate().toString().padStart(2, "0");
-		const month = (fullDate.getMonth() + 1).toString().padStart(2, "0");
-		const year = fullDate.getFullYear().toString();
-
-		return `${day}/${month}/${year}`;
-	};
-
-	const filterTask = (task: ITask) => {
-		const matchStatus = statusMap[task.status].match(new RegExp(filter, "i"));
-		if (matchStatus) return task;
-
-		const matchTitle = task.title.match(new RegExp(filter, "i"));
-		if (matchTitle) return task;
-
-		const matchDate = formatDate(task.createdAt).includes(filter);
-		if (matchDate) return task;
-
-		const matchTag = task.tags.find((tag) => tag.name.match(new RegExp(filter, "i")));
-		if (matchTag) return task;
-	};
-
-	// todo > Component for empty tasks
-	if (!active?.tasks) return null;
-
 	return (
-		<StyledMain data-cy="task-container">
-			{active.tasks
-				.filter((task) => filterTask(task))
-				.map((task) => (
-					<div onClick={() => dispatch(setActive(task))} key={task.id} className="task" data-cy={`task-${task.id}`}>
-						<div className="top">
-							<h1 data-cy={`task-${task.id}-title`} className="title">
-								{task.title}
-							</h1>
-							<span className="date">{formatDate(task.createdAt)}</span>
-							<span data-cy={`task-${task.id}-status`} className={`status ${task.status.toLowerCase()}`}>
-								{statusMap[task.status]}
-							</span>
-						</div>
-
-						<p data-cy={`task-${task.id}-description`} className="description">
-							{task.description}
-						</p>
-
-						<div className="tags" data-cy={`task-${task.id}-tags`}>
-							{task.tags.map((tag) => (
-								<span className="tag" key={tag.id} style={{ backgroundColor: tagColors[tag.color] }}>
-									{tag.name}
-								</span>
-							))}
-						</div>
-					</div>
-				))}
+		<StyledMain>
+			<TaskContainer />
+			{/* <TaskDetails /> */}
 		</StyledMain>
 	);
 };
