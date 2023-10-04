8b6a3b3be7990fe6308fbd2051de199b64243393
Rafael <rafaeloliveiracds@gmail.com>
2023-10-04 17:45:15 -0300

    [ Feat ] Update task mutation

--- frontend/src/features/home/components/main/task-details.tsx
+++ frontend/src/features/home/components/main/task-details.tsx
@@ -4,8 +4,10 @@ import { TaskFields } from "../task-fields";
 import { Store } from "@/context/store";
 import { useRef, useState, useEffect } from "react";
 import { setActive } from "../../context/task-slice";
-import { TaskInput } from "@/services/interfaces/task";
+import { IUpdateTask, RUpdateTask, TaskInput } from "@/services/interfaces/task";
 import { defaultTaskValues } from "../header/create-task";
+import { useMutation } from "@apollo/client";
+import { taskQueries } from "@/services/queries/task";
 
 export const TaskDetails = () => {
 	const { activeTask } = useSelector((state: Store) => state.task);
@@ -15,28 +17,48 @@ export const TaskDetails = () => {
 
 	const dispatch = useDispatch();
 	const setIsOpen = () => dispatch(setActive(null));
+	const [updateTaskMutation, { loading }] = useMutation<RUpdateTask, IUpdateTask>(taskQueries.UPDATE_TASK);
 
 	useEffect(() => {
 		if (activeTask) {
 			const { title, description, status, tags } = activeTask;
 			setTask({ title, description, status, tags });
 		}
+
+		setError("");
 	}, [activeTask]);
 
-	const saveChanges = () => {
+	if (!activeTask) return null;
+
+	const showError = (error: string) => {
+		titleRef.current?.focus();
+		setError(error);
+	};
+
+	const saveChanges = async () => {
+		console.log(task, activeTask);
+
+		if (!task.title) return showError("Sua tarefa precisa de um titulo.");
+		if (task.title.length > 100) return showError("Seu titulo não deve exceder 100 caracteres");
+		setError("");
+
+		try {
+			const tags = task.tags.map(({ name, color }) => ({ name, color }));
+			const variables: IUpdateTask = { updateTaskData: { ...task, tags, taskID: activeTask.id } };
+			const { data } = await updateTaskMutation({ variables });
+			console.log({ data });
+		} catch (error: any) {
+			console.log({ error });
+		}
+
 		// todo >
-		// interface input and response
 		// mutation
 		// catch errors
 		// update state
 		// notification
 		// close active task
 		// tests
-
-		console.log(task, activeTask);
 	};
-
-	if (!activeTask) return null;
 
 	return (
 		<TaskFields props={{ containerTitle: activeTask.title, error, setIsOpen, setTask, task, titleRef }}>
